655daab071f03522bc15c0de0f4c3289b7d272c2
DEBORAH LOI <deboeasy92@gmail.com>
2023-04-28 16:36:02 -0400

    inventory list set up in database

---
+++ routes/inventoryRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const inventoryController = require('../controllers/inventoryController');
+
+router.route('/').get(inventoryController.index);
+
+module.exports = router;
+
+
