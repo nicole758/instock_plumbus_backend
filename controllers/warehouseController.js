5fa42f06c7bbd25c3b0eb2a15d5cb07cde44926e
AndrewVillalba <andrewvillalba1@gmail.com>
2023-04-30 13:49:49 -0400

    added post functionality and knex and uuid

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,3 +1,6 @@
+const knex = require('knex')(require('../knexfile'));
+const { v4: uuidv4 } = require('uuid');
+
 exports.updateWarehouse = (req, res) => {
     knex('warehouses')
         .update(req.body)
@@ -9,3 +12,22 @@ exports.updateWarehouse = (req, res) => {
             res.status(400).send(`Error updating Warehouse ${req.params.id} ${err}`)
         );
 };
+
+exports.addWarehouse = (req, res) => {
+    // Validate the request body for required data
+    if (!req.body.warehouse_name || !req.body.address || !req.body.city|| !req.body.country || !req.body.contact_name || !req.body.contact_position || !req.body.contact_phone || !req.body.contact_email) {
+        return res.status(400).send('Please make sure to provide name,address,city,country,contactName,position,phoneNumber,email fields in a request');
+    }
+
+    const id = uuidv4();
+    knex('warehouses')
+        .insert({...req.body, id })
+        .then(() => {
+            // For POST requests we need to respond with 201 and the location of the newly created record
+            const newWarehouseURL = `/warehouses/${id}`;
+            res.status(201).location(newWarehouseURL).send(newWarehouseURL);
+        })
+        .catch((err) => res.status(400).send(`Error creating Warehouse: ${err}`));
+};
+
+
