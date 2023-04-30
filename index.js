d17c630125b1a826170d16113c51e26794094e81
tenr <grodriguez1224@gmail.com>
2023-04-30 17:27:43 -0400

    adding cors and middleware

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5050;
-const inventoryRoutes = require('./routes/inventoryRoutes');
-const warehouseRoutes = require('./routes/warehouseRoutes');
-
-
+const inventoryRoutes = require("./routes/inventoryRoutes");
+const warehouseRoutes = require("./routes/warehouseRoutes");
+const cors = require("cors");
 
 app.use(express.json());
-app.use('/warehouses', warehouseRoutes);
-app.use('/inventories', inventoryRoutes);
+app.use(cors());
 
+app.use("/warehouses", warehouseRoutes);
+app.use("/inventories", inventoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
