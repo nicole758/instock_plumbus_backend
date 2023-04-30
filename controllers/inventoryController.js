39a7382d12eb5af29247f459295d96ba0bc430e9
AndrewVillalba <127067649+AndrewVillalba@users.noreply.github.com>
2023-04-30 14:01:38 -0400

    Revert "Jbii 26"

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,29 +1,28 @@
-const knex = require("knex")(require("../knexfile"));
+const knex = require('knex')(require('../knexfile'));
 
 exports.index = (_req, res) => {
-  knex("inventories")
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving Inventories: ${err}`)
-    );
+    knex('inventories')
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving Inventories: ${err}`)
+        );
 };
 
 exports.singleInventory = (req, res) => {
-  knex("inventories")
-    .where({ id: req.params.id })
-    .then((data) => {
-      // If record is not found, respond with 404
-      if (!data.length) {
-        return res
-          .status(404)
-          .send(`Record with id: ${req.params.id} is not found`);
-      }
-      // Knex returns an array of records, so we need to send response with a single object only
-      res.status(200).json(data[0]);
-    })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving inventory ${req.params.id} ${err}`)
-    );
+    knex('inventories')
+        .where({ id: req.params.id })
+        .then((data) => {
+            // If record is not found, respond with 404
+            if (!data.length) {
+                return res.status(404).send(`Record with id: ${req.params.id} is not found`);
+            }
+            // Knex returns an array of records, so we need to send response with a single object only
+            res.status(200).json(data[0]);
+        })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving inventory ${req.params.id} ${err}`)
+        );
 };
+
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,10 +1,8 @@
-const router = require("express").Router();
-const inventoryController = require("../controllers/inventoryController");
+const router = require('express').Router();
+const inventoryController = require('../controllers/inventoryController');
 
-//Root fetch
-router.route("/").get(inventoryController.index);
-
-//By ID fetch
-router.route("/:id").get(inventoryController.singleInventory);
+router.route('/').get(inventoryController.index);
 
 module.exports = router;
+
+
