ded65ef7c5fe2cad3651b7e95e52337c2c5f59a6
AndrewVillalba <andrewvillalba1@gmail.com>
2023-04-30 13:59:51 -0400

    fixed typo in index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5050;
 const inventoryRoutes = require('./routes/inventoryRoutes');
-const warehouseRoutes = require('./routes/warehouseRoute');
+const warehouseRoutes = require('./routes/warehouseRoutes');
 
 
 
